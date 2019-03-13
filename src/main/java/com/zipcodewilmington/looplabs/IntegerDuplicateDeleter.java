506dcae362a92f18d142ee1a0d375ddc07216c76
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-03-13 18:28:04 -0400

    Finished integer duplicate deleter

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,62 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.List;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        int indexesToBeRemoved = 0;
+        for (int i = 0; i < array.length; i++){
+            if (countFrequency(array[i]) >= maxNumberOfDuplications){
+                indexesToBeRemoved++;
+            }
+        }
+        Integer[] duplicatesRemoved = new Integer[array.length - indexesToBeRemoved];
+        Integer indexInNewArray = 0;
+        for (int i = 0; i < array.length; i++){
+            if (countFrequency(array[i]) < maxNumberOfDuplications){
+                duplicatesRemoved[indexInNewArray] = array[i];
+                indexInNewArray++;
+            }
+        }
+        return duplicatesRemoved;
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        int indexesToBeRemoved = 0;
+        for (int i = 0; i < array.length; i++){
+            if (countFrequency(array[i]) == exactNumberOfDuplications){
+                indexesToBeRemoved++;
+            }
+        }
+        Integer[] duplicatesRemoved = new Integer[array.length - indexesToBeRemoved];
+        Integer indexInNewArray = 0;
+        for (int i = 0; i < array.length; i++){
+            if (countFrequency(array[i]) != exactNumberOfDuplications){
+                duplicatesRemoved[indexInNewArray] = array[i];
+                indexInNewArray++;
+            }
+        }
+        return duplicatesRemoved;
+    }
+
+    public Integer countFrequency(Integer index){
+        Integer frequency = 0;
+        for (int i = 0; i < array.length; i++){
+            if (array[i] == index){
+                frequency++;
+            }
+        }
+        return frequency;
+    }
 }
